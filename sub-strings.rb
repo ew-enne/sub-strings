3a66b053dd90df7d10d24df1d74cc78489eacca1
Ewenne <221577967+ew-enne@users.noreply.github.com>
2025-11-14 10:16:07 +0100

    Build the complete method
    This finalizes the project.

--- sub-strings.rb
+++ sub-strings.rb
@@ -2,13 +2,20 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings (dictionary)
   puts 'Enter a string to check'
-  string_to_check = gets.chomp
+  string_to_check = gets.downcase.chomp
+  array_of_string = string_to_check.split
+  hash = Hash.new
   dictionary.each do |word|
-    word_counter = 0
-    if string_to_check.include? word
-      word_counter += 1
+  word_counter = 0  
+    array_of_string.each do |array_word|      
+      if array_word.include? word
+        word_counter += 1
+      end
+      if word_counter > 0
+        hash[word] = word_counter
+      end
     end
-    puts "#{word} : counted #{word_counter} times."
+    puts hash
   end
 end
 
